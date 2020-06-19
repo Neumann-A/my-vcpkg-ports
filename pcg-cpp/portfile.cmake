71a90277e192362aa1107fabf4370d987ef6631c
Alexander Neumann <Alexander.Neumann@hamburg.de>
2020-06-19 17:25:01 +0200

    [pcg-cpp] add custom pcg-cpp

---
+++ pcg-cpp/portfile.cmake
@@ -0,0 +1,31 @@
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO Neumann-A/pcg-cpp
+    REF 9f3944e83e85ca1cb29c7962681143876cd75dd7
+    SHA512 11f176a5cddad17959541fe52a19d129a3fe160d010b4405ac3de0617d7dbf7773604a51a0f9d7aeb1bae5259fb27f7a320c3960daf040d6ef35fc3830d8dc94
+    HEAD_REF vs_2017fix_and_cmake
+)
+
+VCPKG_CONFIGURE_CMAKE(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS
+)
+
+vcpkg_install_cmake()
+
+if("${VCPKG_CMAKE_SYSTEM_NAME}" STREQUAL "Linux")
+vcpkg_fixup_cmake_targets(CONFIG_PATH "lib/cmake")
+else()
+vcpkg_fixup_cmake_targets(CONFIG_PATH "cmake")
+endif()
+
+# Put the licence file where vcpkg expects it
+file(COPY ${SOURCE_PATH}/license-mit.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/pcg-cpp)
+file(RENAME ${CURRENT_PACKAGES_DIR}/share/pcg-cpp/license-mit.txt ${CURRENT_PACKAGES_DIR}/share/pcg-cpp/copyright)
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/lib)
+# Copy the pcg-cpp header files
+file(INSTALL ${SOURCE_PATH}/include DESTINATION ${CURRENT_PACKAGES_DIR} FILES_MATCHING PATTERN "*.hpp")
