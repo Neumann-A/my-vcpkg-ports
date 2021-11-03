af58299ce9f2b48f3e713dc6a5fdd05ee6f42800
Alexander Neumann <neumann@imt.uni-luebeck.de>
2021-11-03 11:01:18 +0100

    fix FindMatlab

--- matlab/portfile.cmake
+++ matlab/portfile.cmake
@@ -1,12 +1,14 @@
 SET(VCPKG_POLICY_EMPTY_PACKAGE enabled)
 
-
 #COMPONENTS: MAIN_PROGRAM MAT_LIBRARY ENG_LIBRARY MEX_COMPILER 
 
 # Make sure MATLAB can be found
-vcpkg_configure_cmake(SOURCE_PATH ${CMAKE_CURRENT_LIST_DIR}
+vcpkg_configure_cmake(SOURCE_PATH "${CMAKE_CURRENT_LIST_DIR}"
                       OPTIONS 
                       --trace-expand
                       "-DMatlab_MAIN_PROGRAM=-NOTFOUND"
                       "-D_matlab_main_tmp=-NOTFOUND"
                       "-DCOMPONENTS=${COMPONENTS}")
+
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/FindMatlab.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
