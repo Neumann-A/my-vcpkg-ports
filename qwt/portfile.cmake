9ed7970b91c7c21a88c80b04fa9f5a7d6b19ac9d
Alexander Neumann <neumann@imt.uni-luebeck.de>
2021-12-14 11:14:25 +0100

    add qwt

--- qwt/portfile.cmake
+++ qwt/portfile.cmake
@@ -2,31 +2,32 @@ vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL "https://git.code.sf.net/p/qwt/git"
     REF "a9ac6b28ee990f5d51ea36523057a5af54875e2e"
+    PATCHES 
+        config.patch
+        fix_dll_install.patch
 )
 
+string(COMPARE EQUAL  "${VCPKG_LIBRARY_LINKAGE}" "dynamic" IS_STATIC)
+set(OPTIONS "")
+if(IS_STATIC)
+    set(OPTIONS "QWT_CONFIG+=QwtDll")
+endif()
 vcpkg_qmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
+    QMAKE_OPTIONS
+        ${OPTIONS}
+        "CONFIG-=debug_and_release"
+        "CONFIG+=create_prl"
+        "CONFIG+=link_prl"
+        #QWT_CONFIG += QwtDesigner
+        #QWT_CONFIG += QwtPkgConfig
 )
-
-if (VCPKG_TARGET_IS_WINDOWS)
-    vcpkg_install_qmake(
-        RELEASE_TARGETS sub-src-release_ordered
-        DEBUG_TARGETS sub-src-debug_ordered
-    )
-else ()
-    vcpkg_install_qmake(
-        RELEASE_TARGETS sub-src-all-ordered
-        DEBUG_TARGETS sub-src-all-ordered
-    )
-endif()
+vcpkg_qmake_install()
+vcpkg_copy_pdbs()
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
     file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
 endif()
-
-#Install the header files
-file(GLOB HEADER_FILES "${SOURCE_PATH}/src/*.h")
-file(INSTALL ${HEADER_FILES} DESTINATION "${CURRENT_PACKAGES_DIR}/include/${PORT}")
 
 # Handle copyright
 file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
