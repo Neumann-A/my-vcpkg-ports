305fbed92ff17a987b7f1c3c11d5b04b44ec1690
Alexander Neumann <alexander.neumann@hamburg.de>
2022-05-31 16:28:56 +0200

    add features to hpx

--- hpx/portfile.cmake
+++ hpx/portfile.cmake
@@ -19,15 +19,28 @@ vcpkg_from_github(
 )
 
 set(HPX_WITH_MALLOC system)
-if(VCPKG_TARGET_IS_LINUX)
-    # This is done at the request of the hpx maintainers; see
-    # https://github.com/microsoft/vcpkg/pull/21673#issuecomment-979904882
-    # It must match when gperftools is treated as a dependency of this port.
-    set(HPX_WITH_MALLOC tcmalloc)
-elseif(VCPKG_TARGET_IS_WINDOWS)
-    set(HPX_WITH_MALLOC system)
+if("jemalloc" IN_LIST FEATURES)
+    # jemalloc is preferred to tcmalloc according to the tutorial materials
+    # and since it supports more platforms. 
+    set(HPX_WITH_MALLOC jemalloc) # To avoid memory fragmentation in highly task based applications. 
 endif()
 
+vcpkg_check_features(
+    OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+    "zlib"              HPX_WITH_COMPRESSION_ZLIB
+    "snappy"            HPX_WITH_COMPRESSION_SNAPPY
+    "bzip2"             HPX_WITH_COMPRESSION_BZIP2
+    "cuda"              HPX_WITH_CUDA
+    "mpi"               HPX_WITH_PARCELPORT_MPI
+    "mpi"               HPX_WITH_PARCELPORT_MPI_MULTITHREADED
+)
+
+if(NOT VCPKG_TARGET_ARCHITECTURE MATCHES "(x64|x86)")
+    list(APPEND FEATURE_OPTIONS "-DHPX_WITH_GENERIC_CONTEXT_COROUTINES=ON")
+endif()
+
+file(REMOVE "${SOURCE_PATH}/cmake/FindBZip2.cmake")
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
@@ -35,29 +48,26 @@ vcpkg_cmake_configure(
         -DHPX_WITH_VCPKG=ON
         -DHPX_WITH_TESTS=OFF
         -DHPX_WITH_EXAMPLES=OFF
-        -DHPX_WITH_TOOLS=OFF
+        -DHPX_WITH_TOOLS=ON
         -DHPX_WITH_RUNTIME=OFF
         -DHPX_USE_CMAKE_CXX_STANDARD=ON
-        -DHPX_WITH_COMPRESSION_BZIP2=ON
-        -DHPX_WITH_COMPRESSION_SNAPPY=ON
-        -DHPX_WITH_COMPRESSION_ZLIB=ON
-        -DHPX_WITH_CUDA=OFF
+        ${FEATURE_OPTIONS}
         -DHPX_WITH_PKGCONFIG=OFF
         -DHPX_WITH_STATIC_LINKING=${HPX_WITH_STATIC_LINKING}
-        "-DHPX_WITH_MALLOC=${HPX_WITH_MALLOC}"
+        "-DHPX_WITH_MALLOC:STRING=${HPX_WITH_MALLOC}"
         #-DHPX_WITH_APEX=ON
-        -DHPX_WITH_TOOLS=ON
-        -DHPX_WITH_PARCELPORT_MPI=ON
-        #-DHPX_WITH_PARCELPORT_TCP=ON
+        -DHPX_WITH_PARCELPORT_TCP=ON
+        -DHPX_WITH_THREAD_TARGET_ADDRESS=ON
+        -DHPX_WITH_CHECK_MODULE_DEPENDENCIES=ON
     OPTIONS_DEBUG
+        -DHPX_WITH_THREAD_LOCAL_STORAGE=ON
+        -DHPX_WITH_THREAD_IDLE_RATES=ON
         -DHPX_WITH_COROUTINE_COUNTERS=ON
         -DHPX_WITH_SPINLOCK_DEADLOCK_DETECTION=ON
 )
 # 
-# HPX_WITH_MALLOC should be set
+# HPX_WITH_MODULES_AS_STATIC_LIBRARIES ON by default
 # HPX_WITH_APEX
-# HPX_WITH_GENERIC_CONTEXT_COROUTINES for ARM
-# HPX_WITH_MAX_CPU_COUNT default 64
 vcpkg_cmake_install()
 
 # post build cleanup
