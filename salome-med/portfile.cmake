2485c4ba127477929963f5d05459d2008df9ac13
Alexander Neumann <alexander.neumann@hamburg.de>
2023-04-14 15:02:35 +0200

    more salome stuff

--- salome-med/portfile.cmake
+++ salome-med/portfile.cmake
@@ -1,15 +1,11 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
-    URL "https://git.salome-platform.org/gitpub/modules/geom.git"
-    REF "abd699810f0d9758075b9a4158d94d4965b06539"
-    PATCHES fix-newer-opencascada.patch
+    URL "https://git.salome-platform.org/gitpub/modules/med.git"
+    REF "d0a1739f565d2e6cb47002ea650094895f4bbfd4"
+    PATCHES
 )
 
-string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static"  SMESH_BUILD_STATIC)
-
-vcpkg_find_acquire_program(SWIG)
-cmake_path(GET SWIG PARENT_PATH SWIG_DIR)
-vcpkg_add_to_path("${SWIG_DIR}")
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static"  MEDCOUPLING_BUILD_STATIC)
 
 set(ENV{PYTHONPATH} "${CURRENT_HOST_INSTALLED_DIR}/tools/python3/Lib;${CURRENT_INSTALLED_DIR}/lib/python3.10/site-packages")
 
@@ -20,14 +16,18 @@ vcpkg_cmake_configure(
     OPTIONS 
       "-DCONFIGURATION_ROOT_DIR=${SALOME_CONFIGURATION_ROOT_DIR}"
       "-DKERNEL_ROOT_DIR:PATH=${CURRENT_INSTALLED_DIR}"
+      "-DMEDCOUPLING_ROOT_DIR=${CURRENT_INSTALLED_DIR}"
       -DPYTHON_EXECUTABLE=${CURRENT_HOST_INSTALLED_DIR}/tools/python3/python${VCPKG_HOST_EXECUTABLE_SUFFIX}
       -DSALOME_USE_64BIT_IDS=${SALOME_USE_64BIT_IDS}
       -DSALOME_BUILD_TESTS=OFF
       -DSALOME_BUILD_DOC=OFF
       -DSALOME_BUILD_GUI=OFF
-      -DSALOME_GEOM_USE_VTK=ON
-      "-DOpenCASCADE_INCLUDE_DIR=${CURRENT_INSTALLED_DIR}/include/opencascade"
-      -DOPENCASCADE_ROOT_DIR=${CURRENT_INSTALLED_DIR}/share/opencascade
+      -DSALOME_FIELDS_ENABLE_PYTHON=OFF
+      -DSALOME_INSTALL_SCRIPT_DATA:PATH=share/salome/script
+      -DSALOME_INSTALL_SCRIPT_PYTHON:PATH=share/salome/script
+      -DSALOME_INSTALL_SCRIPT_SCRIPTS:PATH=share/salome/script
+      -DSALOME_INSTALL_BINS:PATH=tools/salome/bin
+      -DSALOME_INSTALL_LIBS:PATH=lib
 )
 
 vcpkg_cmake_install()
@@ -48,35 +48,23 @@ if(NOT VCPKG_BUILD_TYPE)
   endforeach()
 endif()
 
-file(GLOB idl_pys "${CURRENT_PACKAGES_DIR}/bin/salome/*idl.py")
-foreach(idl_py IN LISTS idl_pys)
-  file(READ "${idl_py}" contents)
-  string(REPLACE "r\"${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/idl" "os.path.realpath(os.path.dirname(__file__)) + \"/../../../../idl/salome" contents "${contents}")
-  string(REPLACE "${CURRENT_BUILDTREES_DIR}" "" contents "${contents}")
-  file(WRITE "${idl_py}" "${contents}")
-endforeach()
-
-vcpkg_cmake_config_fixup(PACKAGE_NAME SalomeGEOM CONFIG_PATH "adm_local/cmake_files")
-vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/share/salomegeom/SalomeGEOMConfig.cmake" "/adm_local/cmake_files" "/share/SalomeGEOM")
-
+vcpkg_cmake_config_fixup(PACKAGE_NAME SalomeFIELDS CONFIG_PATH "adm_local/cmake_files")
+vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/share/SalomeFIELDS/SalomeFIELDSConfig.cmake" "/adm_local/cmake_files" "/share/SalomeFIELDS")
 if(VCPKG_TARGET_IS_WINDOWS)
-  set(file "${CURRENT_PACKAGES_DIR}/share/SalomeGEOM/SalomeGEOMTargets-release.cmake")
+  set(file "${CURRENT_PACKAGES_DIR}/share/SalomeFIELDS/SalomeFIELDSTargets-release.cmake")
   file(READ "${file}" contents)
   string(REGEX REPLACE "/lib/([^.]+)\\.dll" "/bin/\\1.dll" contents "${contents}")
   file(WRITE "${file}" "${contents}")
 
   if(NOT VCPKG_BUILD_TYPE)
-    set(file "${CURRENT_PACKAGES_DIR}/share/SalomeGEOM/SalomeGEOMTargets-debug.cmake")
+    set(file "${CURRENT_PACKAGES_DIR}/share/SalomeFIELDS/SalomeFIELDSTargets-debug.cmake")
     file(READ "${file}" contents)
     string(REGEX REPLACE "/lib/([^.]+)\\.dll" "/bin/\\1.dll" contents "${contents}")
     file(WRITE "${file}" "${contents}")
   endif()
 endif()
 
-file(RENAME "${CURRENT_PACKAGES_DIR}/bin/salome" "${CURRENT_PACKAGES_DIR}/tools/salome/bin/salome")
-
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING")
 
 file(REMOVE "${CURRENT_PACKAGES_DIR}/tools/salome/bin/VERSION")
-
-
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/share/salome/script/test")
