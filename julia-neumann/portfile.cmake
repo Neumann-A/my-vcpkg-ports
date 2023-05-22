c2652d85ffa23f9edf6ba9269416ca240d666046
Alexander Neumann <alexander.neumann@hamburg.de>
2023-05-22 15:28:40 +0200

    julia cmake wip

---
+++ julia-neumann/portfile.cmake
@@ -0,0 +1,46 @@
+#vcpkg_from_github(
+#    OUT_SOURCE_PATH SOURCE_PATH
+#    REPO Neumann-A/julia
+#    REF 
+#    SHA512 
+#    HEAD_REF master
+#)
+
+vcpkg_find_acquire_program(PYTHON3)
+#x_vcpkg_get_python_packages(PYTHON_EXECUTABLE "${PYTHON3}" PACKAGES html5lib)
+get_filename_component(PYTHON3_DIR "${PYTHON3}" DIRECTORY)
+vcpkg_add_to_path("${PYTHON3_DIR}")
+vcpkg_find_acquire_program(PERL)
+#x_vcpkg_get_python_packages(PYTHON_EXECUTABLE "${PYTHON3}" PACKAGES html5lib)
+get_filename_component(PERL_DIR "${PERL}" DIRECTORY)
+vcpkg_add_to_path("${PERL_DIR}")
+
+vcpkg_cmake_get_vars(cmake_vars_file)
+include("${cmake_vars_file}")
+if(VCPKG_DETECTED_CMAKE_C_COMPILER_ID STREQUAL "MSVC")
+    if(VCPKG_TARGET_ARCHITECTURE STREQUAL "x86")
+        string(APPEND asmflags " --target=i686-pc-windows-msvc")
+    elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
+        string(APPEND asmflags " --target=x86_64-pc-windows-msvc")
+    elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "arm64")
+        string(APPEND asmflags " --target=arm64-pc-windows-msvc")
+    endif()
+    vcpkg_find_acquire_program(CLANG)
+    set(ccas "${CLANG}")
+else()
+    set(ccas "${VCPKG_DETECTED_CMAKE_C_COMPILER}")
+endif()
+
+set(cmakeopts 
+        "-DCMAKE_ASM_COMPILER=${ccas}"
+        "-DCMAKE_ASM_FLAGS=${asmflags}"
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "E:/src/julia"
+    OPTIONS 
+        ${cmakeopts}
+        #--trace-expand
+)
+vcpkg_cmake_install(ADD_BIN_TO_PATH
+)
