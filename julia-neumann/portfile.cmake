bf946f7b705d057c695c1c4fad233c8483c1d4db
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2023-05-30 23:01:27 +0200

    julia wip

--- julia-neumann/portfile.cmake
+++ julia-neumann/portfile.cmake
@@ -11,9 +11,11 @@ vcpkg_find_acquire_program(PYTHON3)
 get_filename_component(PYTHON3_DIR "${PYTHON3}" DIRECTORY)
 vcpkg_add_to_path("${PYTHON3_DIR}")
 vcpkg_find_acquire_program(PERL)
-#x_vcpkg_get_python_packages(PYTHON_EXECUTABLE "${PYTHON3}" PACKAGES html5lib)
 get_filename_component(PERL_DIR "${PERL}" DIRECTORY)
 vcpkg_add_to_path("${PERL_DIR}")
+vcpkg_find_acquire_program(7Z)
+get_filename_component(7Z_DIR "${7Z}" DIRECTORY)
+vcpkg_add_to_path("${7Z_DIR}")
 
 vcpkg_cmake_get_vars(cmake_vars_file)
 include("${cmake_vars_file}")
@@ -36,8 +38,39 @@ set(cmakeopts
         "-DCMAKE_ASM_FLAGS=${asmflags}"
 )
 
+set(SOURCE_PATH "E:/julia")
+set(ENV{LBT_DEFAULT_LIBS} "mkl_rt.2.dll")
+# file(GLOB jl_stdlib_version_files "${SOURCE_PATH}/stdlib/*.version")
+# foreach(jl_stdlib_version_file IN LISTS jl_stdlib_version_files)
+    # file(STRINGS "${jl_stdlib_version_file}" stdlib_version_info)
+    # cmake_path(GET jl_stdlib_version_file STEM stdlib_name)
+
+    # set(stdlib_git_url "${stdlib_version_info}")
+    # list(FILTER stdlib_git_url INCLUDE REGEX "_GIT_URL")
+    # string(REGEX REPLACE "[^=]+=[ \t]+" "" stdlib_git_url "${stdlib_git_url}")
+
+    # set(stdlib_sha1_ref "${stdlib_version_info}")
+    # list(FILTER stdlib_sha1_ref INCLUDE REGEX "_SHA1")
+    # string(REGEX REPLACE "[^=]+=[ \t]+" "" stdlib_sha1_ref "${stdlib_sha1_ref}")
+
+    # set(stdlib_branch "${stdlib_version_info}")
+    # list(FILTER stdlib_branch INCLUDE REGEX "_BRANCH")
+    # string(REGEX REPLACE "[^=]+=[ \t]+" "" stdlib_branch "${stdlib_branch}")
+
+    # string(TOLOWER "${stdlib_name}" stdlib_lower_name)
+
+    # vcpkg_from_git(
+      # OUT_SOURCE_PATH SOURCE_PATH_${stdlib_name}
+      # URL "${stdlib_git_url}"
+      # REF "${stdlib_sha1_ref}"
+    # )
+    
+    # #file(COPY "${SOURCE_PATH_${stdlib_name}}/" DESTINATION "${SOURCE_PATH}/stdlib/${stdlib_name}")
+# endforeach()
+
+#list(APPEND VCPKG_CMAKE_CONFIGURE_OPTIONS "-DFETCHCONTENT_FULLY_DISCONNECTED=OFF")
 vcpkg_cmake_configure(
-    SOURCE_PATH "E:/src/julia"
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS 
         ${cmakeopts}
         #--trace-expand
