689954c1a2e61992e2e86a76950f599630753748
Alexander Neumann <alexander.neumann@hamburg.de>
2023-11-02 16:11:15 +0100

    disable openmp for msvc

--- magma/portfile.cmake
+++ magma/portfile.cmake
@@ -11,46 +11,8 @@ vcpkg_download_distfile(
 vcpkg_extract_source_archive(
     src_path
     ARCHIVE "${dist_file}"
-    #[NO_REMOVE_ONE_LEVEL]
-    #[SKIP_PATCH_CHECK]
-    #[PATCHES <patch>...]
-    #[SOURCE_BASE <base>]
-    #[BASE_DIRECTORY <relative-path> | WORKING_DIRECTORY <absolute-path>]
+    PATCHES disable-openmp-msvc.patch
 )
-
-#vcpkg_from_bitbucket(
-#    OUT_SOURCE_PATH src_path
-#    REPO ICL/MAGMA
-#    REF "v${VERSION}"
-#    SHA512 b4d20901c21a7d60b2cf74592f4ae34c555ca8ffca29c1331c5d891ab2ee2cfcde739b78fd9319816b41317ca47b8b5dcfd1032a1502d718a75ebd185f091420
-#    HEAD_REF master
-#)
-
-#if(CMAKE_HOST_WIN32)
-#  vcpkg_acquire_msys(msys_root PACKAGES bash make)
-#  set(bash_cmd "${msys_root}/usr/bin/bash.exe")
-#  set(make_cmd "${msys_root}/usr/bin/make.exe")
-#else()
-#  set(make_cmd make)
-#endif()
-
-#set(ENV{GPU_TARGET} Pascal Volta Turing Ampere)
-
-#vcpkg_find_acquire_program(PYTHON3)
-
-#execute_process(
-#  COMMAND ${PYTHON3} "${src_path}/tools/codegen.py"
-#  WORKING_DIRECTORY "${src_path}"
-#)
-
-#file(WRITE "${src_path}/make.inc" "GPU_TARGET = Volta\nFORT = true\n")
-
-#vcpkg_add_to_path("${msys_root}/usr/bin")
-
-#execute_process(
-#  COMMAND ${make_cmd} -d --trace -f ${src_path}/Makefile generate
-#  WORKING_DIRECTORY "${src_path}"
-#)
 
 vcpkg_cmake_configure(
   SOURCE_PATH "${src_path}"
