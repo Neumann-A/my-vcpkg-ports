b434c2533e4b35c6af08f6e67c2b17ab9534c133
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2023-12-16 15:27:09 +0100

    add pyside

--- pyside/portfile.cmake
+++ pyside/portfile.cmake
@@ -1,10 +1,12 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO qtproject/pyside-pyside-setup
-    REF 392559ed8c4d6a733a72af9c1311b8cb2ff3e633
-    SHA512 4d6b9aa76cf9e199fc3209f5a44befcf7ae084fcaca1b9632393e3230a487375aadf42125be62960c3e5ecf027e952d50473f4fbd0ef7f0d664e513e8df159cd
+    REF v${VERSION}
+    SHA512 5ec1fca08b450d734beaf61d32e125f717cfaddcff5942c4ea01a68d355019814e5940b1bd569615b3036673b80e5138dda63b723fb9214ceb54be571d2cd998
     HEAD_REF master
-    PATCHES fix_build.patch
+    PATCHES 
+      fix_build.patch
+      fixes.patch
 )
 
 set(ENV{LLVM_INSTALL_DIR} "${CURRENT_HOST_INSTALLED_DIR}/tools/llvm")
@@ -15,17 +17,28 @@ vcpkg_cmake_configure(
       #-DSHIBOKEN_BUILD_TOOLS=OFF
       -DBUILD_TESTS=OFF
       -DNO_QT_TOOLS=yes
+      #-DPYTHON_LIMITED_API=OFF
+      #-DSHIBOKEN_PYTHON_LIMITED_API=OFF
+      -DFORCE_LIMITED_API=OFF
 )
+
 
 vcpkg_cmake_install(ADD_BIN_TO_PATH)
 vcpkg_cmake_config_fixup(PACKAGE_NAME Shiboken6Tools CONFIG_PATH lib/cmake/Shiboken6Tools DO_NOT_DELETE_PARENT_CONFIG_PATH)
-vcpkg_cmake_config_fixup(PACKAGE_NAME Shiboken6 CONFIG_PATH lib/cmake/Shiboken6-6.4.3 DO_NOT_DELETE_PARENT_CONFIG_PATH)
-vcpkg_cmake_config_fixup(PACKAGE_NAME PySide6 CONFIG_PATH lib/cmake/PySide6-6.4.3 DO_NOT_DELETE_PARENT_CONFIG_PATH)
-vcpkg_cmake_config_fixup(PACKAGE_NAME PySide6Qml CONFIG_PATH lib/cmake/PySide6Qml-6.4.3 DO_NOT_DELETE_PARENT_CONFIG_PATH)
+vcpkg_cmake_config_fixup(PACKAGE_NAME Shiboken6 CONFIG_PATH lib/cmake/Shiboken6 DO_NOT_DELETE_PARENT_CONFIG_PATH)
+vcpkg_cmake_config_fixup(PACKAGE_NAME PySide6 CONFIG_PATH lib/cmake/PySide6 DO_NOT_DELETE_PARENT_CONFIG_PATH)
+vcpkg_cmake_config_fixup(PACKAGE_NAME PySide6Qml CONFIG_PATH lib/cmake/PySide6Qml DO_NOT_DELETE_PARENT_CONFIG_PATH)
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib/cmake")
 if(NOT VCPKG_BUILD_TYPE)
   file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/lib/cmake")
 endif()
+
+set(abi cp311-win_amd64) # abi3 if limited
+
+
+vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/share/Shiboken6/Shiboken6Config.${abi}.cmake" "/Lib/site-packages/" "/tools/python3/Lib/site-packages/")
+vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/share/PySide6/PySide6Config.${abi}.cmake" "/Lib/site-packages/" "/tools/python3/Lib/site-packages/")
+
 
 vcpkg_fixup_pkgconfig()
 #uic rcc qmltyperegistrar qmlimportscanner lrelease lupdate qmllint qmlformat
@@ -40,9 +53,10 @@ set(py_scripts
       qml.py
       qtpy2cpp.py
       deploy.py
-      deploy
+      deploy_lib
       project
       qtpy2cpp_lib
+      android_deploy.py
 )
 foreach(script IN LISTS py_scripts)
   file(RENAME "${CURRENT_PACKAGES_DIR}/bin/${script}" "${CURRENT_PACKAGES_DIR}/tools/${PORT}/${script}")
@@ -54,7 +68,10 @@ endforeach()
 file(GLOB lics "${SOURCE_PATH}/LICENSES/*")
 vcpkg_install_copyright(FILE_LIST ${lics})
 
-vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/Lib/site-packages/PySide6/__init__.py" "${CURRENT_BUILDTREES_DIR}" "do-not-use-and-force-false")
+vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/lib/site-packages/PySide6/__init__.py" "${CURRENT_BUILDTREES_DIR}" "do-not-use-and-force-false")
+
+file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/tools/python3/Lib/")
+file(RENAME "${CURRENT_PACKAGES_DIR}/lib/site-packages" "${CURRENT_PACKAGES_DIR}/tools/python3/Lib/site-packages")
 
 #TODO: Need to add "os.add_dll_directory(os.path.dirname(__file__)+'/../../../bin')" to "vcpkg\installed\triplet\lib\site-packages\shiboken6\__init__.py" before "from shiboken6.Shiboken import *"
 
