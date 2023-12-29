cc76e2a33b0c310b2859223b35c4560b7e4df596
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2023-12-29 19:31:59 +0100

    finetune magma port

--- magma/portfile.cmake
+++ magma/portfile.cmake
@@ -1,12 +1,17 @@
-vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
+set(opts "")
+if(VCPKG_TARGET_IS_WINDOWS AND VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
+  set(opts
+    -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS:BOOL=ON
+    -DCMAKE_CUDA_SEPARABLE_COMPILATION:BOOL=OFF
+  )
+endif()
 
 vcpkg_download_distfile(
     dist_file
-    URLS https://icl.utk.edu/projectsfiles/magma/downloads/magma-2.7.2.tar.gz
+    URLS https://icl.utk.edu/projectsfiles/magma/downloads/magma-${VERSION}.tar.gz
     FILENAME magma-${VERSION}.tar.gz
     SHA512 7ab52ad09f452f7b997da573f74465d5bc8c83392f724747b131a7015b1445c457defdb59ae7a2fd4930e2cdc5bce3c7b99a069f04db1752a5df36ddc6e84987
-    PATCHES
-      fix-cuda.patch
 )
 
 vcpkg_extract_source_archive(
@@ -15,16 +20,16 @@ vcpkg_extract_source_archive(
     PATCHES 
       disable-openmp-msvc.patch
       no-tests.patch
+      clang-cuda.patch
 )
 
 vcpkg_cmake_configure(
   SOURCE_PATH "${src_path}"
   OPTIONS
-    #--trace-expand
-    -DMAGMA_ENABLE_CUDA=ON 
-    -DMAGMA_ENABLE_HIP=OFF # HIP is backend and seems additive !
+    -DMAGMA_ENABLE_CUDA=ON
+    -DMAGMA_ENABLE_HIP=OFF # HIP is backend and seems additive?!
     -DUSE_FORTRAN=OFF
-    
+    ${opts}
 )
 
 vcpkg_cmake_install()
