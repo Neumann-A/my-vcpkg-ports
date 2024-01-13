ef444240e2177394c4b3eef75bcfabd15b204345
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2024-01-13 16:27:10 +0100

    torchaudio/torchvisin have parallel build issues

--- torchaudio/portfile.cmake
+++ torchaudio/portfile.cmake
@@ -4,7 +4,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO pytorch/audio
     REF "v${VERSION}"
-    SHA512 6860e91b87d1ce5b08149dc0192ad5c895520f75c7220bed52b3de64b3e87e9ec39c6fd6596cdf7f5d989e7821e9c04f253112f980a07107eb7f7eaf608d325f
+    SHA512 b7b6d849246f5fb9be78c5fc5aa41dbd9f240bafe827db0f547dfb9f43b52a87ce9813bdb7ef886e0880b0f30d4f212a066da1dbe10075e2786b681fdf746307
     HEAD_REF main
     PATCHES 
       fix-build.patch
@@ -23,6 +23,7 @@ if(VCPKG_TARGET_IS_WINDOWS)
   list(APPEND FEATURE_OPTIONS -DBUILD_SOX=OFF)
 endif()
 
+set(VCPKG_CONCURRENCY 2)
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
@@ -39,6 +40,7 @@ file(REMOVE_RECURSE
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 
 file(COPY "${SOURCE_PATH}/torchaudio" DESTINATION "${CURRENT_PACKAGES_DIR}/tools/python3/Lib/site-packages/")
-file(REMOVE "${CURRENT_PACKAGES_DIR}/tools/python3/Lib/site-packages/torchaudio/lib")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/tools/python3/Lib/site-packages/torchaudio/lib")
 file(RENAME "${CURRENT_PACKAGES_DIR}/lib/" "${CURRENT_PACKAGES_DIR}/tools/python3/Lib/site-packages/torchaudio/lib")
 
+set(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled)
--- torchvision/portfile.cmake
+++ torchvision/portfile.cmake
@@ -18,7 +18,7 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
 if(VCPKG_TARGET_IS_OSX)
     list(APPEND FEATURE_OPTIONS -DWITH_MPS=ON)
 endif()
-
+set(VCPKG_CONCURRENCY 2)
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
