6a231ebdb45dc873b43936dbf2de39855fa3055e
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2024-05-17 11:13:56 +0200

    More import scripts refinements

--- python3/vcpkg-port-config.cmake
+++ python3/vcpkg-port-config.cmake
@@ -23,25 +23,52 @@ function(vcpkg_python_test_import)
     "arg"
     ""
     "MODULE"
-    ""
+    "MODULES;DLL_SEARCH_PATHS"
   )
 
   if(VCPKG_CROSSCOMPILING OR VCPKG_CRT_LINKAGE STREQUAL "static")
     return()
   endif()
 
-  message(STATUS "Testing import of python module '${arg_MODULE}' ...")
+  if(arg_MODULE)
+    list(APPEND arg_MODULES "${arg_MODULE}")
+  endif()
 
-  set(PYTHONPATH_BACKUP "")
-  
+  list(APPEND arg_DLL_SEARCH_PATHS "${CURRENT_INSTALLED_DIR}/bin")
+  set(PYTHON_DLL_SEARCH_DIRS "")
+  foreach(dll_path IN LISTS arg_DLL_SEARCH_PATHS)
+    if(NOT EXISTS "${dll_path}")
+      message(FATAL_ERROR "Path '${dll_path}' in DLL_SEARCH_PATHS does not exist!")
+    endif()
+    string(APPEND PYTHON_DLL_SEARCH_DIRS "  os.add_dll_directory('${dll_path}')\n")
+  endforeach()
+
+  set(PYTHON_IMPORTS "")
+  foreach(module IN LISTS arg_MODULES)
+    string(APPEND PYTHON_IMPORTS "from ${module} import *\n")
+  endforeach()
+
+  unset(pythonpath_bak)
+  if(DEFINED ENV{PYTHONPATH})
+    set(pythonpath_bak "$ENV{PYTHONPATH}")
+  endif()
+
   set(ENV{PYTHONPATH} "${CURRENT_PACKAGES_DIR}/${PYTHON3_SITE}")
 
-  set(DLL_DIR "${CURRENT_INSTALLED_DIR}/bin")
+  message(STATUS "Testing import of python module '${arg_MODULES}' ...")
+
   configure_file("${CMAKE_CURRENT_FUNCTION_LIST_DIR}/import_test.py.in" "${CURRENT_BUILDTREES_DIR}/import_test.py" @ONLY)
 
   vcpkg_execute_required_process(COMMAND "${VCPKG_PYTHON3_EXECUTABLE}" "${CURRENT_BUILDTREES_DIR}/import_test.py"
     LOGNAME "python-import-${arg_MODULE}-${TARGET_TRIPLET}"
     WORKING_DIRECTORY "${CURRENT_BUILDTREES_DIR}"
   )
-  message(STATUS "Import of '${arg_MODULE}' successful")
+  message(STATUS "Import of '${arg_MODULES}' successful")
+
+  if(DEFINED pythonpath_bak)
+    set(ENV{PYTHONPATH} "${pythonpath_bak}")
+  else()
+    unset(ENV{PYTHONPATH})
+  endif()
+
 endfunction()
